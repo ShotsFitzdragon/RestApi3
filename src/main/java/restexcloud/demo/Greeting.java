f6cfedbb2769e65fc97d7f68b24a5f79a04fa94f
Michael Reyes <michaelreyes@jerrys-mbp-2.home>
2019-10-30 18:30:51 -0700

    Please Work

---
+++ src/main/java/restexcloud/demo/Greeting.java
@@ -0,0 +1,34 @@
+package restexcloud.demo;
+
+public class Greeting
+{
+    private final long id;
+    private String content;
+
+    public Greeting()
+    {
+        this.id = 0;
+        this.content = "";
+    }
+
+    public Greeting (long id, String content)
+    {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId()
+    {
+        return id;
+    }
+
+    public void setContent(String content)
+    {
+        this.content = content;
+    }
+
+    public String getContent()
+    {
+        return content;
+    }
+}
